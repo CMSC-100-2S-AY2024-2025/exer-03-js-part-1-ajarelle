891b9fc033b08b5eb3343188a0c742de693e131d
ajarelle <avrivera1@up.edu.ph>
2025-02-14 12:31:55 +0800

    Feat: Added the password validation function

---
+++ index.js
@@ -0,0 +1,38 @@
+// Exercise 3: Basics
+// Adrian Jarelle V. Rivera
+// Description:
+// This program contains three functions that checks and validate a password, 
+// reverses a string and storing it into an object.
+
+//References:
+// https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
+// https://www.geeksforgeeks.org/how-to-check-case-of-letter-in-javascript/
+
+
+function validatePassword(pass1, pass2){
+    
+    if (pass1 !== pass2) { // Checks if the two passwords are equal
+        return false;
+    }
+
+    if (pass1.length < 8) { // checks if their is at least 8 characters
+        return false;
+    }
+
+    const chars = pass1.split(""); // splits the password into an array
+ 
+    const hasUpper = chars.some(char => /[A-Z]/.test(char)); // Checks the password array if it has an Uppercase Letter
+    const hasLower = chars.some(char => /[a-z]/.test(char)); // Checks the password array if it has a Lowercase Letter
+    const hasNumeric = chars.some(char => !isNaN(char) && char !== " "); // Checks if the password array has numeric 
+
+    return hasUpper && hasLower && hasNumeric; 
+}
+
+// Sample Cases
+console.log(validatePassword("helloworld", "hello"));
+console.log(validatePassword("hello", "hello"));
+console.log(validatePassword("hello1234", "hello1234"));
+console.log(validatePassword("Hello1234", "Hello1234"));
+console.log(validatePassword("HELLO1234", "HELLO1234"));
+
+
